f1a06d5424764d83012728739332da2e2ac40405
gian brunner <gian.brunner@ntb.ch>
2018-03-11 17:23:53 +0100

    V1

---
+++ app/controllers/CardController.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+//import services.DefaultCardService;
+
+public class CardController extends Controller{
+
+
+    public Result getCards(String name){
+        if(name.equals("Hans")){
+            return ok("changeCard with Name works!");
+        }
+        else{
+            return ok("changeCard works!");
+        }
+    }
+    public Result createNewCard(){
+        return ok("CreateNewCard works!");
+    }
+    public Result changeCard(Long id){
+        if(id==5){
+            return ok("changeCard with ID works!");
+        }
+        else{
+            return ok("changeCard works!");
+        }
+    }
+    public Result getCard(Long id){
+        if(id==5){
+            return ok("getCard with ID works!");
+        }
+        else{
+            return ok("getCard works!");
+        }
+    }
+    public Result deleteCard(Long id){
+        if(id==5){
+            return ok("deleteCard with ID works!");
+        }
+        else{
+            return ok("deleteCard works!");
+        }
+    }
+}
---
+++ app/models/Index.java
@@ -0,0 +1,43 @@
+package models;
+
+import java.util.List;
+
+public class Index {
+    private Long id;
+    private List<Card> cards;
+    private String name;
+    private String topic;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
