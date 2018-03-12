cb1056320939d621cf51fd899ad08f3b67d2260a
Santoro <santoro@Santoros-MacBook-Pro.local>
2018-03-12 15:02:52 +0100

    Swagger documentation added.

--- app/controllers/CardController.java
+++ app/controllers/CardController.java
@@ -1,18 +1,25 @@
 package controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import play.*;
 import play.mvc.*;
 //import services.DefaultCardService;
 
+@Api(value = "Card Controller", produces = "application/json")
 public class CardController extends Controller{
 
-
+    @ApiOperation(value = "Get Cards", notes = "Get list of cards filtered by string.")
     public Result getCards(String name){
         return ok("changeCard with Name works!");
     }
+
+    @ApiOperation(value = "Create Card", notes = "Create a new Card.")
     public Result createNewCard(){
         return ok("CreateNewCard works!");
     }
+
+    @ApiOperation(value = "Change Card", notes = "Change a card with given id.")
     public Result changeCard(Long id){
         if(id==5){
             return ok("changeCard with ID works!");
@@ -21,6 +28,8 @@ public class CardController extends Controller{
             return ok("changeCard works!");
         }
     }
+
+    @ApiOperation(value = "Get Card", notes = "Get the card with given id.")
     public Result getCard(Long id){
         if(id==5){
             return ok("getCard with ID works!");
@@ -29,6 +38,8 @@ public class CardController extends Controller{
             return ok("getCard works!");
         }
     }
+
+    @ApiOperation(value = "Delete Card", notes = "Delete card with given id.")
     public Result deleteCard(Long id){
         if(id==5){
             return ok("deleteCard with ID works!");
---
+++ app/controllers/CollectionController.java
@@ -0,0 +1,50 @@
+package controllers;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import play.*;
+import play.mvc.*;
+
+@Api(value = "Collection Controller", produces = "application/json")
+public class CollectionController extends Controller{
+
+    @ApiOperation(value = "Get Collections", notes = "Get list of collections filtered by string.")
+    public Result getCollections(String name){
+        return ok("getCollections with Name works!");
+    }
+
+    @ApiOperation(value = "Create Collection", notes = "Create a new collection.")
+    public Result createNewCollection(){
+        return ok("CreateNewCollection works!");
+    }
+
+    @ApiOperation(value = "Change Collection", notes = "Change the collection with given id.")
+    public Result changeCollection(Long id){
+        if(id==5){
+            return ok("changeCollection with ID works!");
+        }
+        else{
+            return ok("changeCollection works!");
+        }
+    }
+
+    @ApiOperation(value = "Get Collection", notes = "Get the collection with given id.")
+    public Result getCollectionById(Long id){
+        if(id==5){
+            return ok("getCollection with ID works!");
+        }
+        else{
+            return ok("getCollection works!");
+        }
+    }
+
+    @ApiOperation(value = "Delete Collection", notes = "Delete the collection with given id.")
+    public Result deleteCollection(Long id){
+        if(id==5){
+            return ok("deleteCollection with ID works!");
+        }
+        else{
+            return ok("deleteIndex works!");
+        }
+    }
+}
