c64c99daac5af63e9936d617211ae7679eb5bc71
Santoro <santoro@Santoros-MacBook-Pro.local>
2018-03-12 17:01:01 +0100

    Added Card Service Interface and Class

--- app/controllers/CollectionController.java
+++ app/controllers/CollectionController.java
@@ -4,32 +4,30 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import play.*;
 import play.mvc.*;
+import services.DefaultCollectionService;
 
 @Api(value = "Collection Controller", produces = "application/json")
 public class CollectionController extends Controller{
+
+    DefaultCollectionService defCollService = new DefaultCollectionService();
 
     @ApiOperation(value = "Get Collections", notes = "Get list of collections filtered by string.")
     public Result getCollections(String name){
         return ok("getCollections with Name works!");
     }
 
-    @ApiOperation(value = "Create Collection", notes = "Create a new collection.")
+    @ApiOperation(value = "Create Collection", notes = "Create a new collection from json.")
     public Result createNewCollection(){
         return ok("CreateNewCollection works!");
     }
 
-    @ApiOperation(value = "Change Collection", notes = "Change the collection with given id.")
-    public Result changeCollection(int id){
-        if(id==5){
-            return ok("changeCollection with ID works!");
-        }
-        else{
-            return ok("changeCollection works!");
-        }
+    @ApiOperation(value = "Change Collection", notes = "Change the collection with collection from json.")
+    public Result changeCollection(){
+        return ok("changeCollection works!");
     }
 
     @ApiOperation(value = "Get Collection", notes = "Get the collection with given id.")
-    public Result getCollectionById(int id){
+    public Result getCollection(int id){
         if(id==5){
             return ok("getCollection with ID works!");
         }
