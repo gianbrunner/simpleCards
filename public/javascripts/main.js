30d25ca914764a2570712cb9920370ac1b10a1c4
Santoro <santoro@Santoros-MacBook-Pro.local>
2018-05-11 15:28:43 +0200

    backend

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -30,40 +30,47 @@ function card(context) {
         type: "GET",
         dataType : "json"
     }).done(function(json) {
-//Cards für jede Collection erzeugen
+//Layout erzeugen
         console.log(json);
         context.render('/assets/html/cards.html', {})
             .appendTo(context.$element())
+            .then(function(){
+                var layout =    '<div class="container" id="chooseCollection">'+
+                                '<h2>Kartei auswählen</h2>'+
+                                '</div>'+
+                                '<div class="container" id="questionAnswer">'+
+                                '<h2>Frage und Antwort formulieren</h2>'+
+                                '</div>';
+                $(".cards").append(layout);
+            })
+//Cards für jede Collection erzeugen
             .then(function () {
                 json = $.makeArray(json);
                 $.each(json, function(index, value) {
-                    var card =  '<div class="card" style="width: 20rem;">'+
+                    var card =  '<div class="col-sm-6 col-lg-3">'+
+                                '<div class="card" id="'+ value.id +'">'+
                                 '<div class="card-header">'+ value.name +'</div>' +
                                 '<ul class="list-group list-group-flush">' +
                                 '<li class="list-group-item">'+ value.topic +'</li>' +
                                 '<li class="list-group-item">'+ value.description +'</li>' +
-                                '</ul></div>';
-                    $(".cards").append(card);
+                                '</ul></div></div>';
+                    $("#chooseCollection").append(card);
                 });
+            })
+//Frage und Antwort erzeugen
+            .then(function () {
+                var questionAnswer ='<form>' +
+                                    '<div class="form-group">' +
+                                    '<input type="text" class="form-control" id="colQuestion" placeholder="Frage">' +
+                                    '</div>' +
+                                    '<div class="form-group">' +
+                                    '<input type="text" class="form-control" id="colAnswer" placeholder="Antwort">' +
+                                    '</div>' +
+                                    '<button id="colSubmit" type="submit" class="btn btn-info">Anlegen</button>' +
+                                    '</form>';
+                    $("#questionAnswer").append(questionAnswer);
             });
     });
-//Frage-&Antwortfeld
-    context.render('/assets/html/homepage.html', {})
-        .appendTo(context.$element())
-        .then(function () {
-            var frageAntwort =  '<form>' +
-                                '<div class="form-group">' +
-                                '<input type="text" class="form-control" id="colQuestion" placeholder="Name">' +
-                                '</div>' +
-                                '<div class="form-group">' +
-                                '<textarea class="form-control" id="colAnswer" placeholder="Beschreibung" rows="4"></textarea>' +
-                                '</div>' +
-                                '<div id="popup">Kartei wurde angelegt</div>' +
-                                '<button id="colSubmit" type="submit" class="btn btn-info">Anlegen</button>' +
-                                '</form>';
-
-                $(".cards").append(frageAntwort);
-        });
 }
 
 function collection(context) {
