428118373cf964a7f411ae5b7167e91ee726d8e8
gian brunner <gian.brunner@ntb.ch>
2018-05-20 12:58:21 +0200

    main.js augesplitted

---
+++ public/javascripts/utils/collections.js
@@ -0,0 +1,55 @@
+function collection(context) {
+    context.render('/assets/html/collections.html', {})
+        .appendTo(context.$element())
+        .then(function () {
+            let layout =
+                '<h2 class="title">Kartei anlegen</h2>' +
+                '<form>' +
+                '<div class="form-group">' +
+                '<input type="text" class="form-control" id="colName" placeholder="Name">' +
+                '</div>' +
+                '<div class="form-group">' +
+                '<input type="text" class="form-control" id="colTopic" placeholder="Thema">' +
+                '</div>' +
+                '<div class="form-group">' +
+                '<textarea class="form-control" id="colDescr" placeholder="Beschreibung" rows="4"></textarea>' +
+                '</div>' +
+                '<div id="popup">Kartei wurde angelegt</div>' +
+                '<button id="colSubmit" type="submit" class="btn btn-info">Anlegen</button>' +
+                '</form>';
+            $(".collections").append(layout);
+            $('#popup').hide();
+        })
+        .then(function () {
+            document.getElementById('colSubmit').addEventListener('click', colAdd);
+        });
+}
+
+function colAdd() {
+    let colName = $('#colName').val();
+    let colTopic = $('#colTopic').val();
+    let colDescr = $('#colDescr').val();
+    let col = {
+        name: colName,
+        topic: colTopic,
+        description: colDescr
+    };
+    // let ref = $('#colSubmit');
+    // let popup = $('#popper');
+    // popup.show();
+    // var popper = new Popper(ref, popup, {
+    //     placement: 'right'
+    // });
+    col = JSON.stringify(col);
+    console.log(col);
+    var url = '/api/collections';
+    $.ajax({
+        url: url,
+        data: col,
+        method: "POST",
+        dataType: "json",
+        contentType: "json"
+    }).done(function (json) {
+        console.log("done");
+    });
+}
---
+++ public/javascripts/utils/homepage.js
@@ -0,0 +1,14 @@
+function homepage(context) {
+    context.render('/assets/html/homepage.html', {})
+        .appendTo(context.$element())
+        .then(function () {
+            var jumbotron = '<div class="jumbotron">' +
+                '<h1 class="diplay-3">Willkommen bei simpleCards!</h1>' +
+                '<p class="lead">Diese Webapplikation hilft dir, Karteikarten zu erstellen, zu ordnen und zu lernen, ganz ohne Papier.</p>' +
+                '<hr class="my-4">' +
+                '<p class="lead">Du kannst problemlos mehrere Karteien zu deinen Schulfächern erstellen, diese mit Karten füllen und dich dann selbst abfragen. Klingt einfach, ist es auch!</p>' +
+                '<a class="btn btn-primary btn-lg" href="#/collection" role="button">Jetzt beginnen</a>' +
+                '</div>';
+            $(".home").append(jumbotron);
+        });
+}
