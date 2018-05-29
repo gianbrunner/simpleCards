4ec37ced7cb6dfab1068dbac4d5409ae3f8a2b79
gian brunner <gian.brunner@ntb.ch>
2018-05-29 16:45:59 +0200

    learn.js angepasst

--- public/javascripts/utils/learn.js
+++ public/javascripts/utils/learn.js
@@ -1,9 +1,7 @@
 function learn(context) {
 //Collection abfragen
-    var collectionUrl = '/api/collections';
-    var cardUrl = '/api/cards';
     $.ajax({
-        url: collectionUrl,
+        url: '/api/collections',
         method: "GET",
         contentType: "application/json"
     }).done(function(json) {
@@ -11,28 +9,47 @@ function learn(context) {
         console.log(json);
         context.render('/assets/html/learn.html', {})
             .appendTo(context.$element())
-            .then(function(){
-                var layout =    '<div class="container" id="chooseCollection">'+
-                                '<h2>Kartei auswählen</h2>'+
-                                '</div>';
+            .then(function () {
+                var layout = '<div class="container" id="chooseCollection">' +
+                    '<h2>Kartei auswählen</h2>' +
+                    '</div>';
                 $(".learn").append(layout);
             })
             //Liste erzeugen
-            .then(function(){
+            .then(function () {
                 json = $.makeArray(json);
-                var list =  '<form><div class="form-group">'+
-                            '<select class="form-control" id="collectionList">'+
-                            '<option selected>Bitte Kartei auswählen</option>'+
-                            '</select></div></form>';
+                var list = '<form><div class="form-group">' +
+                    '<select class="form-control" id="collectionList">' +
+                    '<option selected>Bitte Kartei auswählen</option>' +
+                    '</select></div></form>';
                 $("#chooseCollection").append(list);
-                $.each(json, function(index, value){
-                    var option = '<option value="'+ value.id +'">'+ value.name +'</option>';
+                $.each(json, function (index, value) {
+                    var option = '<option value="' + value.id + '">' + value.name + '</option>';
                     $("#collectionList").append(option);
                 });
             })
-//Startbutton
-            .then(function(){
-                var startbutton = '<button type="button" class="btn btn-primary">Starten</button>';
-                $("")
-    })
+            //Startbutton
+            .then(function () {
+                var startbutton = '<button type="button" class="btn btn-primary" id="startbutton">Starten</button>';
+                $("#chooseCollection").append(startbutton);
+            })
+            .then(function (){
+                var colID = $("#collectionList").val();
+                document.getElementById('startbutton').addEventListener('click', function(){
+//Fragen anzeigen
+                    $.ajax({
+                        url:'/api/cards/:'+ colID ,
+                        method: "GET",
+                        contentType: "application/json"
+                    }).done(function(json) {
+                        console.log(json);
+                        $("#chooseCollection").hide();
+                        var questionAnswer = '<div class="container" id="questionAnswer">' +
+                            '<p id="question">' + +'</p>'
+                        '</div>';
+                    })
+                });
+            })
+    });
 }
+
