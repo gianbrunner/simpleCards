f07bea18887eb3fadb3056d36c04a24642557abb
gian brunner <gian.brunner@ntb.ch>
2018-05-30 13:46:27 +0200

    learn.js erweitert

--- public/javascripts/utils/learn.js
+++ public/javascripts/utils/learn.js
@@ -30,26 +30,52 @@ function learn(context) {
             })
             //Startbutton
             .then(function () {
-                var startbutton = '<button type="button" class="btn btn-primary" id="startbutton">Starten</button>';
-                $("#chooseCollection").append(startbutton);
+                var startButton = '<button type="button" class="btn btn-primary" id="startButton">Starten</button>';
+                $("#chooseCollection").append(startButton);
             })
-            .then(function (){
-                var colID = $("#collectionList").val();
-                document.getElementById('startbutton').addEventListener('click', function(){
-//Fragen anzeigen
-                    $.ajax({
-                        url:'/api/cards/:'+ colID ,
-                        method: "GET",
-                        contentType: "application/json"
-                    }).done(function(json) {
-                        console.log(json);
-                        $("#chooseCollection").hide();
-                        var questionAnswer = '<div class="container" id="questionAnswer">' +
-                            '<p id="question">' + +'</p>'
+            .then(function () {
+                document.getElementById('startButton').addEventListener('click', showQuestions)
+            });
+        });
+}
+
+function showQuestions(){
+    var colID = $("#collectionList").val();
+    $.ajax({
+        url:'/api/cards',
+        method: "GET",
+        contentType: "application/json"
+    }).done(function(json) {
+        console.log(json);
+        $("#chooseCollection").hide();
+        //Container erzeugen
+        var container = '<div class="container" id="questionAnswer">'+
+                        '<h2>Fragen beantworten</h2>' +
                         '</div>';
-                    })
-                });
-            })
+        $(".learn").append(container);
+        //Fragen und Anworten erzeugen
+        $.each(json, function(index, value){
+            if(value.fk_id == colID){
+                //Frage erzeugen
+                var question = '<p id="question">'+ value.question +'</p>'
+                $("#questionAnswer").append(question);
+                //Antwortfeld erzeugen
+                var answerBox = '<input class="form-control" id="answer" type="text" placeholder="Bitte Antwort eingeben">'+
+                '</input><hr>';
+                $("#questionAnswer").append(answerBox);
+            }
+        });
+        var checkButton = '<button type="button" class="btn btn-primary" id="checkButton">Überprüfen</button>';
+        $("#questionAnswer").append(checkButton);
+    }).then(function () {
+            document.getElementById('checkButton').addEventListener('click', checkAnswers)
     });
+}
+
+function checkAnswers(){
+    //Antworten vergleichen
+    //Statistik anzeigen
+
+    $("#questionAnswer").hide();
 }
 
