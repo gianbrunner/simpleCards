7da8b8f2adbc3e842d82a5c227347e313f07e79b
gian brunner <gian.brunner@ntb.ch>
2018-06-04 15:58:19 +0200

    Änderungen an overview

--- public/javascripts/utils/overview.js
+++ public/javascripts/utils/overview.js
@@ -1,5 +1,6 @@
 // --- globale Variablen
 var colArray = [];
+var url = 'localhost:9000';
 // --- Methoden
 function overview(context) {
     //Collection abfragen
@@ -14,24 +15,37 @@ function overview(context) {
             .appendTo(context.$element())
             .then(function () {
                 var layout = '<div class="container" id="collections">' +
+                    '<div class="media">'+
+                    '<div class="media-body">'+
                     '<h2>Übersicht der Karteien</h2>' +
+                    '</div>'+
+                    '<img src="/assets/images/addButton.svg" id="addColButton" alt="addButton" width="50" height="50">'+
+                    '</div>'+
                     '<div class="row" id="colOverview">'+
                     '</div></div>';
                 $(".overview").append(layout);
+                document.getElementById('addColButton').addEventListener('click', function(){
+                    addCol();
+                });
             })
             //Cards für Collections erzeugen
             .then(function () {
                 $.each(json, function (index, value) {
-                    var card =  '<div class="col-sm-12 col-md-6 col-4"><div class="card"><div class="card-body">'+
+                    var card =  '<div class="col-sm-12 col-lg-6"><div class="card"><div class="card-body">'+
                                 '<h4 class="card-title">'+ value.name +'</h4>'+
                                 '<p class="card-text">Kategorie: '+ value.topic +
                                 '<br>Beschreibung: '+ value.description +'</p>'+
+                                '<div class="btn-group" role="group" aria-label="Basic example">'+
                                 '<button type="button" class="btn btn-primary" id="cardButton'+ value.id +'" value="'+value.id+'">Karten anzeigen</button>'+
+                                '<button type="button" class="btn btn-secondary" id="cardAddButton'+ value.id +'" value="'+value.id+'">Karte hinzufügen</button>'+
                                 '<button type="button" class="btn btn-danger" id="deleteButton'+ value.id +'" value="'+value.id+'">Kartei löschen</button>'+
-                                '</div></div></div>';
+                                '</div></div></div></div>';
                     $("#colOverview").append(card);
                     document.getElementById('cardButton'+ value.id +'').addEventListener('click', function(){
                         loadCards(value.id, value.name);
+                    });
+                    document.getElementById('cardAddButton'+ value.id +'').addEventListener('click', function(){
+                        addCard();
                     });
                     document.getElementById('deleteButton'+ value.id +'').addEventListener('click', function(){
                         deleteCol(value.id);
@@ -74,6 +88,7 @@ function loadCards(colID, colName){
 }
 
 function deleteCol(colID){
+    alert("Kartei gelöscht!");
     $.ajax({
         url: '/api/collections/'+colID,
         type: 'DELETE',
@@ -85,10 +100,20 @@ function deleteCol(colID){
 
 function deleteCard(cardID){
     $.ajax({
-        url: '/api/cards/'+colID,
+        url: '/api/cards/'+cardID,
         type: 'DELETE',
         success: function(result) {
             alert("Karte gelöscht!");
         }
     });
+}
+
+function addCol(){
+    var colUrl = 'http://'+ url + '/#/collection';
+    $(location).attr('href', colUrl);
+}
+
+function addCard(){
+    var cardUrl = 'http://'+ url + '/#/card';
+    $(location).attr('href', cardUrl);
 }
