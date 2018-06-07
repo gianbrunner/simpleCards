d2ce9b74a59615e8fbdf34334b27f5e104fe3501
Santoro <santoro@Santoros-MacBook-Pro.local>
2018-06-07 16:33:20 +0200

    functional-test für CardAPI und Junit-test für Models hinzugefügt

--- test/api/CardAPITest.java
+++ test/api/CardAPITest.java
@@ -1,66 +1,37 @@
 package api;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.fasterxml.jackson.databind.JsonNode;
+import models.Card;
 import org.junit.Test;
+import play.libs.Json;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 import static org.junit.Assert.assertEquals;
 
 
-public class CardApiTest {
+public class CardAPITest {
 
-
+    /**
+     * Opens a HTTP-connection and sends a GET request to the URL=http://localhost:9000/api/cards/1 .
+     * The result is a json-object from the card with id = 1.
+     * The json-object is converted to a card-object.
+     * The card-object parameters are compared with the expected parameters in assertequals-functions.
+     */
     @Test
-    public void testPOSTCard() {
+    public void testGetCardWithID1(){
         try {
-
-            String payload = "{\"question\":\"TestQuestion\",\"answer\":\"TestAnswer\",\"nTries\":\"10\",\"nCorrect\":\"5\",\"category\":{\"id\":\"1\"},\"box\":{\"id\":\"1\"}}";
-            StringEntity entity = new StringEntity(payload,
-                    ContentType.APPLICATION_FORM_URLENCODED);
-
-            HttpClient httpClient = HttpClientBuilder.create().build();
-            HttpPost request = new HttpPost("http://localhost:9000/api/card");
-            request.setEntity(entity);
-            request.setHeader("Content-type", "application/json");
-
-            HttpResponse response = httpClient.execute(request);
-            assertEquals(200, response.getStatusLine().getStatusCode());
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    @Test
-    public void testGETAllCards() {
-        try {
-            URL url = new URL("http://localhost:9000/api/card");
+            URL url = new URL("http://localhost:9000/api/cards/1");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
-
             // Response code
             int responseCode = con.getResponseCode();
-
-
             // The JSON response
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(con.getInputStream())
             );
-
-
             String inputLine;
             StringBuffer response = new StringBuffer();
             while ((inputLine = in.readLine()) != null) {
@@ -68,72 +39,19 @@ public class CardApiTest {
             }
             in.close();
 
-            // Test if responseCode equals 200
-            assertEquals(200, responseCode);
-
-            // Tests:
-            // question equals "Brotaufstrich"
-            // answer equals "spread"
             String responseJsonString = response.toString();
-            JSONArray jsonarray = new JSONArray(responseJsonString);
-            assertEquals("Brotaufstrich", jsonarray.getJSONObject(0).getString("question"));
-            assertEquals("spread", jsonarray.getJSONObject(0).getString("answer"));
+            JsonNode cardJsonNode = Json.parse(responseJsonString);
+            Card dut = Json.fromJson(cardJsonNode, Card.class);
 
+            assertEquals(200, responseCode);
+            assertEquals((Long)1L, dut.getId());
+            assertEquals((Long)1L, dut.getFk_id());
+            assertEquals("Englisches Wort für Apfel?", dut.getQuestion());
+            assertEquals("Apple", dut.getAnswer());
 
         } catch (Exception e) {
             e.printStackTrace();
 
         }
-
-
     }
-
-    @Test
-    public void testGETBoxWithId() {
-        int cardid = 2;
-        try {
-            URL url = new URL("http://localhost:9000/api/card/" + cardid);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-
-            // Response code
-            int responseCode = con.getResponseCode();
-
-
-            //The JSON response
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream())
-            );
-
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            // Test if responseCode equals 200
-            assertEquals(200, responseCode);
-
-
-            // Tests Box with Id 2
-            // question equals "König"
-            // answer equals "king"
-            String responseJsonString = response.toString();
-            JSONObject jsonObj = new JSONObject(responseJsonString);
-            assertEquals("König", jsonObj.getString("question"));
-            assertEquals("king", jsonObj.getString("answer"));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-
-        }
-
-    }
-
-
 }
-
-
-
-
